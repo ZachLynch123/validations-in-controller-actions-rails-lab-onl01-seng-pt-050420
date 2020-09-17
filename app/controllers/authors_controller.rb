fa6f7125996af6713bc0219eafd68b4087133d28
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-09-17 19:58:40 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,9 +7,9 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create!(author_params)
     
-    if @author.valid? 
+    
+    if @author = Author.create!(author_params) 
       @author.save
       redirect_to author_path(@author)
     else 
